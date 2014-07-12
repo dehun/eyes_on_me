48b923bc32acf743313a457d594df83da5194282
Yuriy Netesov <retnuhed@gmail.com>
2014-07-12 14:07:06 +0300

    initial commit

---
+++ eyes_on_me/temperature.py
@@ -0,0 +1,46 @@
+import subprocess
+
+
+kelvin_table = {
+    1000: (255, 56, 0),
+    1500: (255, 109, 0),
+    2000: (255, 137, 18),
+    2500: (255, 161, 72),
+    3000: (255, 180, 107),
+    3500: (255, 196, 137),
+    4000: (255, 209, 163),
+    4500: (255, 219, 186),
+    5000: (255, 228, 206),
+    5500: (255, 236, 224),
+    6000: (255, 243, 239),
+    6500: (255, 249, 253),
+    7000: (245, 243, 255),
+    7500: (235, 238, 255),
+    8000: (227, 233, 255),
+    8500: (220, 229, 255),
+    9000: (214, 225, 255),
+    9500: (208, 222, 255),
+    10000: (204, 219, 255)}
+
+
+def color_temperature_to_rgb(kelvin):
+    normalized_kelvin = int(kelvin) / 500 * 500
+    if (normalized_kelvin < min(kelvin_table.keys())):
+        return kelvin_table[min(kelvin_table.keys())]
+    elif (normalized_kelvin > max(kelvin_table.keys())):
+        return kelvin_table[max(kelvin_table.keys())]
+    else:
+        return kelvin_table[normalized_kelvin]
+
+
+def set_temperature(kalvin):
+    rgb = color_temperature_to_rgb(kalvin)
+    rgb = [float(x) for x in rgb]
+    # normalize around 1.0
+    median = sum(rgb) / len(rgb)
+    normalized_rgb = [x / median for x in rgb]
+    # set via xgamma
+    (r, g, b) = normalized_rgb
+    subprocess.check_call("xgamma -rgamma %s" % r, shell=True)
+    subprocess.check_call("xgamma -ggamma %s" % g, shell=True)
+    subprocess.check_call("xgamma -bgamma %s" % b, shell=True)
